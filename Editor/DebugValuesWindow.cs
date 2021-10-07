c622d178bf8910205fcc2159f1092966f8d11b10
ArturBerk <berkmail@mail.ru>
2021-10-07 12:37:13 +0300

    Debug values first import

---
+++ Editor/DebugValuesWindow.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Concurrent;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Abg.Debug
+{
+    class DebugValuesWindow : EditorWindow
+    {
+        [MenuItem("Window/Debug Values")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(DebugValuesWindow)).titleContent = new GUIContent("Debug Values");
+        }
+
+        public DebugValuesWindow()
+        {
+            titleContent = new GUIContent("Debug Values");
+        }
+
+        private VisualElement Container;
+        private CategoryElement defaultCategory;
+        private ConcurrentBag<(string Name, DebugValues.Category Category)> newCategories =
+            new ConcurrentBag<(string Name, DebugValues.Category Category)>();
+
+        private class CategoryElement : Foldout
+        {
+            private readonly VisualElement Container;
+            private ConcurrentBag<(string Name, DebugValues.ValueInstance Value)> newValues =
+                new ConcurrentBag<(string Name, DebugValues.ValueInstance Value)>();
+
+            public CategoryElement(DebugValues.Category category)
+            {
+                text = category.Name;
+                Add(Container = new VisualElement());
+                category.ValueAdded += CategoryOnValueAdded;
+                foreach (var valuesKey in category.Values)
+                {
+                    AddItemElement(valuesKey.Key, valuesKey.Value);
+                }
+            }
+
+            private void CategoryOnValueAdded(string name, DebugValues.ValueInstance valueInstance)
+            {
+                newValues.Add((name, valueInstance));
+            }
+
+            private void AddItemElement(string name, DebugValues.ValueInstance valueInstance)
+            {
+                var itemHandler = new ItemElement(name, valueInstance, Container.childCount);
+                Container.Add(itemHandler);
+            }
+
+            public void Update()
+            {
+                while (newValues.TryTake(out var valueTuple))
+                {
+                    AddItemElement(valueTuple.Name, valueTuple.Value);
+                }
+
+                if (!value) return;
+                for (int i = 0; i < Container.childCount; i++)
+                {
+                    var item = Container[i] as ItemElement;
+                    item?.Update();
+                }
+            }
+        }
+
+        private class ItemElement : VisualElement
+        {
+            private readonly DebugValues.ValueInstance valueInstance;
+            public readonly int Index;
+            public readonly Label ValueLabel;
+
+            public ItemElement(string name, DebugValues.ValueInstance valueInstance, int index)
+            {
+                this.valueInstance = valueInstance;
+                this.Index = index;
+                style.flexDirection = new StyleEnum<FlexDirection>(FlexDirection.Row);
+                style.paddingBottom = 2;
+                style.paddingTop = 2;
+                // style.backgroundColor = Index % 2 == 0
+                //     ? new StyleColor(Color.Lerp(Color.gray, Color.white, 0.7f))
+                //     : new StyleColor(Color.Lerp(Color.gray, Color.white, 0.6f));
+                var nameLabel = new Label(name);
+                //                NameLabel.style.backgroundColor = new StyleColor(Color.green);
+                nameLabel.style.flexGrow = 1;
+                nameLabel.style.flexBasis = 50;
+                ValueLabel = new Label();
+                //                ValueLabel.style.backgroundColor = new StyleColor(Color.blue);
+                ValueLabel.style.flexGrow = 2;
+                ValueLabel.style.flexBasis = 100;
+                Add(nameLabel);
+                Add(ValueLabel);
+            }
+
+            public void Update()
+            {
+                ValueLabel.text = valueInstance.Value;
+            }
+        }
+
+        private void OnEnable()
+        {
+            const float margin = 5;
+            //DebugValues.Initialize();
+            DebugValues.CategoryAdded += OnCategoryAdded;
+            DebugValues.Cleared += OnCleared;
+            Container = new ScrollView(ScrollViewMode.Vertical)
+            {
+                style =
+                {
+                    marginBottom = margin,
+                    marginTop = margin,
+                    marginLeft = margin,
+                    marginRight = margin
+                }
+            };
+            Container.style.flexGrow = 1;
+            var clearButton = new Button(ClickEvent)
+            {
+                text = "Clear",
+                style =
+                {
+                    marginBottom = margin,
+                    marginLeft = margin,
+                    marginRight = margin
+                }
+            };
+            clearButton.style.flexGrow = 0;
+            rootVisualElement.Add(Container);
+            rootVisualElement.Add(clearButton);
+
+            defaultCategory = new CategoryElement(DebugValues.defaultCategory);
+            Container.Add(defaultCategory);
+            foreach (var category in DebugValues.categories.Values)
+            {
+                Container.Add(new CategoryElement(category));
+            }
+        }
+
+        private void OnCategoryAdded(DebugValues.Category category)
+        {
+            newCategories.Add((name, category));
+        }
+
+        private void OnCleared()
+        {
+            Container.Clear();
+            Container.Add(defaultCategory);
+        }
+
+        private void ClickEvent()
+        {
+            DebugValues.Clear();
+        }
+
+        private float elapsedTime;
+
+        private void Update()
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            if (elapsedTime < 0.1f) return;
+            elapsedTime = 0.0f;
+
+            while (newCategories.TryTake(out var pair))
+            {
+                Container.Add(new CategoryElement(pair.Category));
+            }
+            DebugValues.DispatchValues();
+
+            for (int i = 0; i < Container.childCount; i++)
+            {
+                var categoryElement = Container[i] as CategoryElement;
+                categoryElement?.Update();
+            }
+        }
+    }
+}
---
+++ Runtime/DebugValues.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using UnityEngine;
+
+[assembly: InternalsVisibleTo("Abg.DebugValues.Editor")]
+
+namespace Abg.Debug
+{
+    public class DebugValues
+    {
+        private static Thread mainThread;
+        private static readonly ConcurrentQueue<(string, ValueToDispatch)> valueQueue = new ConcurrentQueue<(string, ValueToDispatch)>();
+        public static readonly Dictionary<string, Category> categories = new Dictionary<string, Category>();
+        public static readonly Category defaultCategory;
+
+        public static event Action<Category> CategoryAdded;
+        public static event Action Cleared;
+
+        static DebugValues()
+        {
+            defaultCategory = new Category("Default");
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            mainThread = Thread.CurrentThread;
+        }
+
+        public static void DispatchValues()
+        {
+            while (valueQueue.TryDequeue(out var value))
+            {
+                GetCategory(value.Item2.Category).SetValue(value.Item2.Name, value.Item2.Value);
+            }
+        }
+
+        private static Category GetCategory(string name)
+        {
+            if (name == null) return defaultCategory;
+            if (!categories.TryGetValue(name, out var category))
+            {
+                category = new Category(name);
+                categories.Add(name, category);
+                CategoryAdded?.Invoke(category);
+            }
+
+            return category;
+        }
+
+        private static string FormattedValue(object value, string format = null)
+        {
+            if (value is string stringValue && format == null) return stringValue;
+            return format != null ? string.Format("{0:" + format + "}", value) : value?.ToString();
+        }
+
+        public static void Show(string name, object value, string format = null)
+        {
+            if (Thread.CurrentThread != mainThread)
+            {
+                valueQueue.Enqueue((name, new ValueToDispatch(null, name, FormattedValue(value, format))));
+                return;
+            }
+            GetCategory(null).SetValue(name, FormattedValue(value, format));
+        }
+
+        public static void ShowAt(object category, string name, object value, string format = null)
+        {
+            if (Thread.CurrentThread != mainThread)
+            {
+                valueQueue.Enqueue((name, new ValueToDispatch(category.ToString(), name, FormattedValue(value, format))));
+                return;
+            }
+
+            GetCategory(category.ToString()).SetValue(name, FormattedValue(value, format));
+        }
+
+        public static void Clear()
+        {
+            defaultCategory.Values.Clear();
+            categories.Clear();
+            Cleared?.Invoke();
+        }
+
+        public class Category
+        {
+            public readonly string Name;
+            public readonly Dictionary<string, ValueInstance> Values = new Dictionary<string, ValueInstance>();
+            public event Action<string, ValueInstance> ValueAdded;
+
+            public Category(string name)
+            {
+                Name = name;
+            }
+
+            public void SetValue(string name, string value)
+            {
+                if (!Values.TryGetValue(name, out var valueInstance))
+                {
+                    valueInstance = new ValueInstance();
+                    Values[name] = valueInstance;
+                    ValueAdded?.Invoke(name, valueInstance);
+                }
+
+                valueInstance.Value = value;
+            }
+        }
+
+        private struct ValueToDispatch
+        {
+            public readonly string Category;
+            public readonly string Name;
+            public readonly string Value;
+
+            public ValueToDispatch(string category, string name, string value)
+            {
+                Category = category;
+                Name = name;
+                Value = value;
+            }
+        }
+
+        public class ValueInstance
+        {
+            public string Value;
+
+            public override string ToString()
+            {
+                return Value;
+            }
+        }
+    }
+}
